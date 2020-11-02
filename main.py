5d595ff52e35ffa0be2a1eb0882309332902f144
Snittz115 <50799835+Snittz115@users.noreply.github.com>
2020-11-02 17:00:27 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def encrypt(phrase):
+    encryption = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            encryption = encryption + "g%$"
+        else:
+            encryption = encryption + letter
+    return (encryption)
+
+
+print(encrypt(input("Type in a phrase>>>>")))
